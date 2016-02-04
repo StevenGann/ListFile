5a1d17f92fe4ef48711e9902f3d38ae1792e1524
Steven Gann <sgann2012@fau.edu>
2016-02-03 20:56:47 -0500

    Ported to C#

---
+++ ListFile/ListFile.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class ListFile<T> : List<T>
+{
+
+    public int Count = 0;
+    private string className;
+    private string directory;
+    private Type c;
+    private int index = 0;
+    private string folder = "data";
+
+    public ListFile()
+    {
+
+    }
+
+    public ListFile(int i, string folder)
+    {
+        this.index = i;
+        this.folder = folder;
+    }
+
+    public ListFile(string folder)
+    {
+        this.folder = folder;
+    }
+
+    public ListFile(int i)
+    {
+        this.index = i;
+    }
+
+
+
+
+
+    new public T this[int index]
+    {
+        get
+        {
+            return read(index);
+        }
+        set
+        {
+            write(index, value);
+        }
+    }
+
+
+    public T get(int index)
+    {
+        return this.read(index);
+
+    }
+
+    private void write(int i, T o)
+    {
+        //TODO make this suck less
+        //I think I made it suck less. ~Steven
+        string dir = System.Environment.GetEnvironmentVariable("USERPROFILE") + "/Desktop/" + "ListFile/" + this.folder + "/" + this.directory + "/";
+
+        if (!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+
+        string filename = className + "." + i + ".xml";
+
+        XmlSerializer writer = new XmlSerializer(typeof(T));
+
+        StreamWriter file = new StreamWriter(dir + filename);
+        writer.Serialize(file, o);
+        file.Close();
+    }
+
+    private T read(int index)
+    {
+        string filename = System.Environment.GetEnvironmentVariable("USERPROFILE") + "/Desktop/" + "ListFile/" + this.folder + "/" + this.directory + "/" + className + "." + index + ".xml";
+        XmlSerializer mySerializer = new XmlSerializer(typeof(T));
+
+        FileStream myFileStream = new FileStream(filename, FileMode.Open);
+        T obj;
+        obj = (T)mySerializer.Deserialize(myFileStream);
+
+        myFileStream.Close();
+        return obj;
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            // Yield each day of the week.
+            yield return read(i);
+        }
+    }
+
+    new public void Add(T o)
+    {
+        this.className = o.GetType().Name;
+        this.directory = className + "." + index;
+        this.c = o.GetType();
+        this.Count += 1;
+        this.write(Count - 1, o);
+
+    }
+
+    new public void Insert(int i, T o)
+    {
+        this.write(i, o);
+    }
+
+    new public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Tester/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListFile<int> testList1 = new ListFile<int>(1);
+            testList1.Add(1);
+            testList1.Add(2);
+            testList1.Add(3);
+            testList1.Add(4);
+            testList1.Add(5);
+
+            ListFile<string> testList2 = new ListFile<string>(1);
+            testList2.Add("one");
+            testList2.Add("two");
+            testList2.Add("three");
+            testList2.Add("four");
+            testList2.Add("five");
+            testList2.Insert(0, "Definitely not one");
+
+            ListFile<int> testList3 = new ListFile<int>(2);
+            testList3.Add(1);
+            testList3.Add(2);
+            testList3.Add(3);
+            testList3.Add(4);
+            testList3.Add(5);
+
+            Console.WriteLine(testList1[0]);
+            Console.WriteLine(testList2[1]);
+            Console.WriteLine(testList3[2]);
+
+            Console.ReadLine();
+        }
+    }
+}
